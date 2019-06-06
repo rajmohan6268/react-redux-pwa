20c8e5589a15ee37fa83c0a93d3d2d7d82d6eb92
Shyam Hajare <hajareshyam@gmail.com>
2019-06-06 19:34:17 +0530

    Updates

---
+++ precache-manifest.3b8382bcad5fec2260102651b46c61b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73cf6b740e4df33dd280288aa2a516a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89f509ad7052d2097caa",
+    "url": "/static/css/main.9b0cec6c.chunk.css"
+  },
+  {
+    "revision": "27ea84db0dc6a3cd353b",
+    "url": "/static/js/2.760bb9d9.chunk.js"
+  },
+  {
+    "revision": "89f509ad7052d2097caa",
+    "url": "/static/js/main.c3656ba5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/static/media/pokeball.f10bf078.png"
+  }
+]);
